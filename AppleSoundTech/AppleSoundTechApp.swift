8031b14432af413e7b9c24eb0b311125e4ea8707
Isla <Islajiin@gmail.com>
2025-07-03 22:30:05 +0900

    Add Xcode project

---
+++ AppleSoundTech/AppleSoundTechApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleSoundTechApp.swift
+//  AppleSoundTech
+//
+//  Created by yeonjin on 7/3/25.
+//
+
+import SwiftUI
+
+@main
+struct AppleSoundTechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
